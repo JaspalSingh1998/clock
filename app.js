992dc918472972db279396f9102d3fdd212e67a6
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-19 10:46:27 +0530

    Finished Clock Application

--- app.js
+++ app.js
@@ -33,12 +33,59 @@ const months = [
 toggle.addEventListener("click", (e) => {
   const html = document.querySelector("html");
   if (html.classList.contains("dark")) {
-    html.classList.add("light");
     html.classList.remove("dark");
     e.target.textContent = "Dark Mode";
   } else {
-    html.classList.remove("light");
     html.classList.add("dark");
     e.target.textContent = "Light Mode";
   }
 });
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hour = time.getHours();
+  const hoursForClock = hour % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hour >= 12 ? "PM" : "AM";
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.textContent = `${hour}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+function scale(num, in_min, in_max, out_min, out_max) {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
